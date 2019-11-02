9e139a005b30d62d6ff39c3bf248a9783dcdffaa
manjeet <manjeet.dhanjal@gmail.com>
2019-11-02 11:49:39 +1100

    created single view application

---
+++ Metro/Metro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Metro
+//
+//  Created by Manjeet Singh on 2/11/19.
+//  Copyright © 2019 iOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
